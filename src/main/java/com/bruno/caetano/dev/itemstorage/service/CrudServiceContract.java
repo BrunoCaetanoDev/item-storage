13251a3fed61ff50aac527b7937d87aafb2fff09
leonormadureira <leonor.madureira1@vodafone.com>
2020-12-16 16:27:08 +0000

    added feature to sort results by a list of fields; refactored links headers construction

--- src/main/java/com/bruno/caetano/dev/itemstorage/service/CrudServiceContract.java
+++ src/main/java/com/bruno/caetano/dev/itemstorage/service/CrudServiceContract.java
@@ -1,18 +1,18 @@
 package com.bruno.caetano.dev.itemstorage.service;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 public interface CrudServiceContract<T extends Object> {
 
-    Page<T> findAll(T entity, PageRequest pageRequest);
+	Page<T> findAll(T entity, Pageable pageRequest);
 
-    T findBydId(String id);
+	T findBydId(String id);
 
-    T save(T item);
+	T save(T item);
 
-    T update(T item);
+	T update(T item);
 
-    void deleteById(String id);
+	void deleteById(String id);
 
 }
--- src/main/java/com/bruno/caetano/dev/itemstorage/service/ItemServiceContract.java
+++ src/main/java/com/bruno/caetano/dev/itemstorage/service/ItemServiceContract.java
@@ -1,14 +1,13 @@
 package com.bruno.caetano.dev.itemstorage.service;
 
 import com.bruno.caetano.dev.itemstorage.entity.model.Item;
-
 import java.math.BigInteger;
 
 public interface ItemServiceContract extends CrudServiceContract<Item> {
 
 
-    void restock(String id, BigInteger qty);
+	void restock(String id, BigInteger qty);
 
-    void dispatch(String id, BigInteger qty);
+	void dispatch(String id, BigInteger qty);
 
 }
