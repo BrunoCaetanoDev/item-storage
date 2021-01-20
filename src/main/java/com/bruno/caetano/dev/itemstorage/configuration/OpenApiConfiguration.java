9011b23bdac1c65ce03e3908515fe67d5301f98b
DESKTOP-BVONRVL\BRUNO <brunoacc92@gmail.com>
2021-01-20 18:08:39 +0000

    removed openapi configuration bean from test profile

--- src/main/java/com/bruno/caetano/dev/itemstorage/configuration/OpenApiConfiguration.java
+++ src/main/java/com/bruno/caetano/dev/itemstorage/configuration/OpenApiConfiguration.java
@@ -8,7 +8,9 @@ import org.springdoc.core.SpringDocUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@Profile("!test")
 @Configuration
 public class OpenApiConfiguration {
 
